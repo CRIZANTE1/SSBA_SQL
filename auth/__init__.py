79a8fe915740c7e869da84c4ec42e428199e642f
CRISTIAN CARLOS <140450586+CRIZANTE1@users.noreply.github.com>
2025-09-20 11:04:21 -0300

    Update __init__.py

--- auth/__init__.py
+++ auth/__init__.py
@@ -1,12 +1,1 @@
-from .auth_utils import (
-    is_user_logged_in,
-    get_user_display_name,
-    get_user_role,
-    check_permission 
-)
 
-from .login_page import (
-    show_login_page,
-    show_user_header,
-    show_logout_button
-)
