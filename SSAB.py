0cb7e91de051d8ef895fa4adba0882aebddbaf89
CRISTIAN CARLOS <140450586+CRIZANTE1@users.noreply.github.com>
2025-09-24 07:22:52 -0300

    Update SSAB.py

--- SSAB.py
+++ SSAB.py
@@ -102,11 +102,8 @@ def main():
         page_to_run["function"]()
 
 if __name__ == "__main__":
-    try:
-        main()
-        st.caption('Copyright 2024, Cristian Ferreira Carlos, Todos os direitos reservados.')
-        st.caption('https://www.linkedin.com/in/cristian-ferreira-carlos-256b19161/')
-    except Exception as e:
-        st.error(f"Erro inesperado no sistema: {str(e)}")
-        st.stop()
+    main()
+    st.caption('Copyright 2024, Cristian Ferreira Carlos, Todos os direitos reservados.')
+    st.caption('https://www.linkedin.com/in/cristian-ferreira-carlos-256b19161/')
+    
 
